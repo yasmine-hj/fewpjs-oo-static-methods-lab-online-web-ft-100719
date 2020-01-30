072e7a177ef822242f05e20e483de64c47f672e1
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-30 22:55:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(string){
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let titleized = []
+    let split = string.split(" ")
+    for (const word of split){
+      if (split.indexOf(word) == 0){
+        titleized.push(this.capitalize(word))
+      } else {
+        exceptions.includes(word) ? titleized.push(word) : titleized.push(this.capitalize(word))      
+      }
+    }
+    return titleized.join(" ")
+  } 
 }
